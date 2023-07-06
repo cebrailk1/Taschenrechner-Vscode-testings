d5d9d733014c5e07e24f437f2266090a63a23c9e
cebrailk1 <138202641+cebrailk1@users.noreply.github.com>
2023-07-06 10:02:09 +0200

    Add files via upload

---
+++ taschenrechner.py
@@ -0,0 +1,15 @@
+rechenart=str(input('Welche Rechenart möchtest du benutzen:'))
+zahl1=int(input('Gebe die erste Zahl ein mit der du rechnen möchtest:'))
+zahl2=int(input('Gebe die zweite Zahl ein mit der du rechnen möchtest:'))
+if rechenart == '+':
+    print(f'{zahl1}{rechenart}{zahl2} = {zahl1+zahl2}')
+elif rechenart == '+':
+    print(f'{zahl1}{rechenart}{zahl2} = {zahl1-zahl2}')
+elif rechenart == '-':
+    print(f'{zahl1}{rechenart}{zahl2} = {zahl1+zahl2}')
+elif rechenart == '*':
+    print(f'{zahl1}{rechenart}{zahl2} = {zahl1*zahl2}')
+elif rechenart == '/':
+    print(f'{zahl1}{rechenart}{zahl2} = {zahl1/zahl2}')
+else:
+    print:('Kein gültiger Operator')
